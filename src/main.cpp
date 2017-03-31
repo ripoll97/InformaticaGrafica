33bbbb375ead191a6f66f4a271c00df6a703e758
ripoll97 <miquel97ripoll@gmail.com>
2017-03-31 13:38:52 +0200

    Exercici 1 (5.1)
    Camera start and rotatio perspective

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ const GLint WIDTH = 800, HEIGHT = 600;
 bool WIDEFRAME = false;
 int Numbuffer = 1;
 float fadeValue = 0;
-int rotationValue;
+int rotationValue = 50;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
 	//cuando se pulsa una tecla escape cerramos la aplicacion
@@ -51,9 +51,9 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 int main() {
 	//initGLFW
-//TODO
-	
-	
+	//TODO
+
+
 	GLFWwindow* window;
 	if (!glfwInit())
 		exit(EXIT_FAILURE);
@@ -64,7 +64,7 @@ int main() {
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
 	//create a window
-//TODO
+	//TODO
 	window = glfwCreateWindow(WIDTH, HEIGHT, "Finestra del projecte", nullptr, nullptr);
 
 	if (!window) {
@@ -74,7 +74,7 @@ int main() {
 	}
 	glfwMakeContextCurrent(window);
 	//set GLEW and inicializate
-//TODO
+	//TODO
 	glewExperimental = GL_TRUE;
 	if (GLEW_OK != glewInit()) {
 		cout << "Error al iniciar glew" << endl;
@@ -82,21 +82,22 @@ int main() {
 		return NULL;
 	}
 	//set function when callback
-//TODO
+	//TODO
 	glfwSetKeyCallback(window, key_callback);
 	//set windows and viewport
-//TODO
+	//TODO
 	int screenWithd, screenHeight;
 	glfwGetFramebufferSize(window, &screenWithd, &screenHeight);
 	//fondo
 	glClear(GL_COLOR_BUFFER_BIT);
 	glClearColor(0.6, 0.0, 0.0, 1.0);
-//TODO
+	//TODO
 
-	
+
 
 	//cargamos los shader
-	Shader shdr("./src/textureVertexShader.vertexshader", "./src/TextureFragment.fragmentshader");
+	//Shader shdr("./src/textureVertexShader.vertexshader", "./src/TextureFragment.fragmentshader");
+	Shader shdr("./src/CubeVertexShader.vertexshader", "./src/CubeFragmentShader.fragmentshader");
 
 	// Definir el buffer de vertices
 
@@ -107,11 +108,68 @@ int main() {
 		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 1.0f, // Bottom Left
 		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 0.0f  // Top Left 
 	};
+
+	GLfloat VertexBufferCube[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f , -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f , -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f , -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f , -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f , -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f , -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f ,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f ,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
 	// Definir el EBO
 
 	GLuint IndexBufferObject[]{
-		0, 1, 3, 
+		0, 1, 3,
 		3, 1, 2
+	};
+
+	vec3 CubesPositionBuffer[] = {
+		vec3(0.0f ,  0.0f,  0.0f),
+		vec3(2.0f ,  5.0f, -15.0f),
+		vec3(-1.5f, -2.2f, -2.5f),
+		vec3(-3.8f, -2.0f, -12.3f),
+		vec3(2.4f , -0.4f, -3.5f),
+		vec3(-1.7f,  3.0f, -7.5f),
+		vec3(1.3f , -2.0f, -2.5f),
+		vec3(1.5f ,  2.0f, -2.5f),
+		vec3(1.5f ,  0.2f, -1.5f),
+		vec3(-1.3f,  1.0f, -1.5f)
 	};
 
 	// Crear los VBO, VAO y EBO
@@ -123,19 +181,19 @@ int main() {
 	glGenBuffers(Numbuffer, &EBO);
 
 	//Establecer el objeto
-		//Declarar el VBO y el EBO
+	//Declarar el VBO y el EBO
 
 
-	
-		//Enlazar el buffer con openGL
+
+	//Enlazar el buffer con openGL
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexBufferObject), VertexBufferObject, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(IndexBufferObject), IndexBufferObject, GL_STATIC_DRAW);
-	
+
 	glBindTexture(GL_TEXTURE_2D, 0);
-		//Establecer las propiedades de los vertices
+	//Establecer las propiedades de los vertices
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)0);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
@@ -143,6 +201,8 @@ int main() {
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
 	glEnableVertexAttribArray(2);
+
+	glEnable(GL_DEPTH_TEST);
 
 	// Texture 1
 
@@ -177,36 +237,51 @@ int main() {
 	SOIL_free_image_data(image2);
 
 
-		//liberar el buffer
+	//liberar el buffer
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindTexture(GL_TEXTURE_2D, 0);
 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-		//liberar el buffer de vertices
-	
+	//liberar el buffer de vertices
+
 	GLint variableShader = glGetUniformLocation(shdr.Program, "shaderVariable");
 	GLfloat variableFade = glGetUniformLocation(shdr.Program, "fade");
-	GLint finalTrans = glGetUniformLocation(shdr.Program, "transformationMatrix");
+	GLint uniModel = glGetUniformLocation(shdr.Program, "model");
+	GLint uniView = glGetUniformLocation(shdr.Program, "view");
+	GLint uniProj = glGetUniformLocation(shdr.Program, "proj");
 
 	//bucle de dibujado
-	while (!glfwWindowShouldClose(window)) 
+	while (!glfwWindowShouldClose(window))
 	{
 
-		mat4 transformationMatrix, escalationMatrix, translationMatrix1, translationMatrix2, rotationMatrix;
+		mat4 transformationMatrix, escalationMatrix, translationMatrix1, translationMatrix2, rotationMatrix, model, view, proj;
 
 		// Definir las matrices de transformacion
-		escalationMatrix = scale(escalationMatrix, vec3(0.5f, 0.5f, 0.0f));
-		translationMatrix1 = translate(translationMatrix1, vec3(0.5f, 0.5f, 0.0f));
-		//translationMatrix2 = translate(translationMatrix2, vec3(0.5f, 0.0f, 0.0f));
-		rotationMatrix = rotate(rotationMatrix, radians(float(rotationValue)), vec3(0.0f, 0.0f, 1.0f));
-		transformationMatrix = translationMatrix1 * rotationMatrix * escalationMatrix;
+		//escalationMatrix = scale(escalationMatrix, vec3(0.5f, 0.5f, 0.0f));
+		translationMatrix1 = translate(translationMatrix1, vec3(0.0f, -0.5f, 0.0f));
+		rotationMatrix = rotate(rotationMatrix, radians(float(rotationValue)), vec3(1.0f, 0.0f, 0.0f));
+
+		model = translationMatrix1 * rotationMatrix * model;
+
+		translationMatrix2 = translate(translationMatrix2, vec3(0.0, 0.0f, -0.3f));
+		// No funciona
+		//view = translationMatrix2 * view;
+
+		// lookAt sacado de Open.gl
+		view = lookAt(
+			vec3(1.2f, 1.2f, 1.2f),
+			vec3(0.0f, 0.0f, 0.0f),
+			vec3(0.0f, 0.0f, 1.0f)
+		);
+
+		proj = perspective(radians(60.0f), 800.0f / 600.0f, 1.0f, -10.0f);
 
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwSetKeyCallback(window, key_callback);
 		//Establecer el color de fondo
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(1.0, 1.0, 0.0, 1.0);
-		
+
 		//establecer el shader
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture);
@@ -231,14 +306,15 @@ int main() {
 			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		}
 
-		glUniformMatrix4fv(finalTrans, 1, GL_FALSE, value_ptr(transformationMatrix));
+		glUniformMatrix4fv(uniModel, 1, GL_FALSE, value_ptr(model));
+		glUniformMatrix4fv(uniView, 1, GL_FALSE, value_ptr(view));
+		glUniformMatrix4fv(uniProj, 1, GL_FALSE, value_ptr(proj));
 
-		glBindVertexArray(0);
-				
 		glBindVertexArray(0);
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 	// liberar la memoria de los VAO, EBO y VBO
 	glDeleteVertexArrays(1, &VAO);
@@ -250,5 +326,5 @@ int main() {
 }
 /*
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-	//TODO, comprobar que la tecla pulsada es escape para cerrar la aplicación y la tecla w para cambiar a modo widwframe
+//TODO, comprobar que la tecla pulsada es escape para cerrar la aplicación y la tecla w para cambiar a modo widwframe
 }*/
